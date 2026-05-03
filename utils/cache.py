32cccd3e938e271fc30302fbef1a7873e988af77
ronv94 <patelronak94@gmail.com>
2026-05-03 14:28:42 -0400

    caching.

--- utils/cache.py
+++ utils/cache.py
@@ -1,9 +1,11 @@
 from flask_caching import Cache
 
-cache = Cache(config={
-    "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 300,
-})
+cache = Cache(
+    config={
+        "CACHE_TYPE": "SimpleCache",
+        "CACHE_DEFAULT_TIMEOUT": 300,
+    }
+)
 
 
 def clear_data_cache():
